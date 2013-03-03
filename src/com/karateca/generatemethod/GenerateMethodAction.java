631aed0764500c9728c782e4eeb43f1f49b54c0e
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-03-03 13:23:12 -0500

    move caret to the function name position

--- src/com/karateca/generatemethod/GenerateMethodAction.java
+++ src/com/karateca/generatemethod/GenerateMethodAction.java
@@ -3,8 +3,6 @@ package com.karateca.generatemethod;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.editor.impl.DocumentImpl;
 import com.intellij.openapi.editor.impl.EditorImpl;
 import com.intellij.openapi.project.Project;
@@ -25,6 +23,7 @@ public class GenerateMethodAction extends AnAction {
 
   @Override
   public void update(AnActionEvent e) {
+    // TODO: disable for non js.
     e.getPresentation().setEnabled(e.getData(PlatformDataKeys.EDITOR) != null);
   }
 
@@ -42,7 +41,11 @@ public class GenerateMethodAction extends AnAction {
       public void stateChanged(ChangeEvent changeEvent) {
         if (changeEvent.getSource().equals("NamespaceFound")) {
           String namespace = namespaceFinder.getNamespaceFound();
-          addMethodRunningCommand(namespace);
+
+          // TODO: show error: "not found"
+          if (namespace != null) {
+            addMethod(namespace);
+          }
         }
       }
     });
@@ -50,19 +53,20 @@ public class GenerateMethodAction extends AnAction {
     namespaceFinder.findText("goog.provide", false);
   }
 
-  private void addMethodRunningCommand(final String namespace) {
+  private void addMethod(final String namespace) {
     CommandUtil.runCommand(project, new Runnable() {
       @Override
       public void run() {
-        addMethod(namespace);
+        int offset = editor.getCaretModel().getOffset();
+
+        String methodTemplate = String.format("%s.prototype. = function() {\n" +
+                "  // TODO: method block\n" +
+                "\n};", namespace);
+        document.replaceString(offset, offset, methodTemplate);
+
+        // Put the caret after "protoype."
+        editor.getCaretModel().moveToOffset(offset + namespace.length() + 11);
       }
     }, "Added method");
-  }
-
-  private void addMethod(String namespace) {
-    int offset = editor.getCaretModel().getOffset();
-
-    String methodTemplate = String.format("%s.prototype.$END$ = function() {\n\n};", namespace);
-    document.replaceString(offset, offset, methodTemplate);
   }
 }
